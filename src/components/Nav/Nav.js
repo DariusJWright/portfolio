eddb582cd2e335461a45ac30ec0c61b1f92a134b
darshnablah <dwright758@gmail.com>
2020-09-29 17:39:23 -0500

    added about and nav bar

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+function Nav() {
+  const links = [
+    {
+      name: 'About Me'
+    },
+    {
+      name: 'Portfolio'
+    },
+    {
+      name: 'Contact'
+    },
+    {
+      name: 'Resumé'
+    }
+  ];
+
+  function linkSelected(name) {
+    console.log(name);
+  }
+
+  return (
+    <header>
+      <h2>
+        <a href='/'>Darius Wright</a>
+      </h2>
+      <nav>
+        <ul>
+          {links.map((link) => (
+            <li key={link.name}>
+              <span onClick={() => linkSelected(link.name)}>{link.name}</span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;

50a328e5bf3e9d02181e20cc1e15f0fb2d96f0b9
darshnablah <dwright758@gmail.com>
2020-10-02 13:30:39 -0500

    form complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About/About';
 import Nav from './components/Nav/Nav';
 import Portfolio from './components/Portfolio/Portfolio';
+import Contact from './components/Contact/Contact'
 
 function App() {
   const [links] = useState([
@@ -24,6 +25,7 @@ function App() {
     <div>
       <Nav links={links}></Nav>
       <main>
+        <Contact />
         <Portfolio></Portfolio>
         <About></About>
       </main>

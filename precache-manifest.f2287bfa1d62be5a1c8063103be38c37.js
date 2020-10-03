92cea5eaa867ca92c2f82f1677cb8788e784a8ac
darshnablah <dwright758@gmail.com>
2020-10-03 15:58:35 -0500

    Updates

---
+++ precache-manifest.f2287bfa1d62be5a1c8063103be38c37.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a126d70b9783c0e00b83d3019da6fb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "882cc35c8287d895d1c4",
+    "url": "/portfolio/static/css/main.83340eb4.chunk.css"
+  },
+  {
+    "revision": "57bb24abc334123644c3",
+    "url": "/portfolio/static/js/2.4088cdf0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.4088cdf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "882cc35c8287d895d1c4",
+    "url": "/portfolio/static/js/main.44b911c7.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "71a079be774cfd579033eee5adf91e4a",
+    "url": "/portfolio/static/media/bored.71a079be.png"
+  },
+  {
+    "revision": "a4617817650fa83c17acd371f038d2dd",
+    "url": "/portfolio/static/media/instartist.a4617817.PNG"
+  },
+  {
+    "revision": "af4a764843d7d40af820796c13458b71",
+    "url": "/portfolio/static/media/linkedin.af4a7648.png"
+  },
+  {
+    "revision": "72fba47ce52080137a38c26376bdb97f",
+    "url": "/portfolio/static/media/me.72fba47c.jpg"
+  },
+  {
+    "revision": "d169aa7d4fcc8abf9013176bad1c0c79",
+    "url": "/portfolio/static/media/note.d169aa7d.png"
+  },
+  {
+    "revision": "20fc329c5eff22e1028396fcb2b1626e",
+    "url": "/portfolio/static/media/resume.20fc329c.txt"
+  },
+  {
+    "revision": "ccf7bb78d457f1d0cc6cf373df173a2c",
+    "url": "/portfolio/static/media/weather.ccf7bb78.png"
+  }
+]);

a00b967ffdbd5aefa5953aae83bee1bb6f30511a
darshnablah <dwright758@gmail.com>
2020-12-15 17:59:00 -0600

    fixed some small things

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Home() {
   return (
-    <h1 className='welcome'>WELCOME TO MY PORTFOLIO!</h1>
+    <div classname='home'>
+      <h1 className='welcome'>WELCOME TO MY PORTFOLIO!</h1>
+    </div>
   )
 }
 
